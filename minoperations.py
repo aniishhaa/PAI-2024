f84ecc333830bba211acbb57773fa4d988af49dd
aniishhaa <156596533+aniishhaa@users.noreply.github.com>
2024-06-26 21:00:55 +0530

    Create minoperations.py

---
+++ minoperations.py
@@ -0,0 +1,7 @@
+    def minimumOperations(self, nums: List[int]) -> int:
+        count=0
+        for num in nums:
+            if num % 3 == 1 or num % 3 == 2:
+                count +=1
+        return count
+        
